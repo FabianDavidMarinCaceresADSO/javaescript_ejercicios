ea39d1c91c9523b857b3ac29ef09bd4989059394
FabianDavidMarinCaceresADSO <rrzarate4@misena.edu.co>
2024-03-09 11:18:35 -0500

    dia 2 de trabajo

---
+++ ejercicio19.js
@@ -0,0 +1,3 @@
+/* 19. Escribir un programa que calcule el área y el volumen de un cilindro.*/
+let radio = parseFloat(prompt("ingrese el radio deñ cilindro"));
+let altura = parseFloat(prompt("ingrese la altura del cilindro"));
